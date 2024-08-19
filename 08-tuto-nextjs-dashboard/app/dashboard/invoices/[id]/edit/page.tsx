d1659667e6efdf59b500a9ee9fdb7e3378774801
Renaud Redron <renaud.redron@gmail.com>
2024-08-19 14:06:49 +0200

    Gestion des erreurs

--- 08-tuto-nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ 08-tuto-nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,8 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
@@ -8,7 +10,12 @@ export default async function Page({ params }: { params: { id: string } }) {
         fetchInvoiceById(id),
         fetchCustomers(),
     ]);
-    
+
+    // Si la page demandé n'ai pas trouvé
+    if (!invoice) {
+        notFound();
+    }
+
     return (
         <main>
         <Breadcrumbs
--- 08-tuto-nextjs-dashboard/app/lib/actions.ts
+++ 08-tuto-nextjs-dashboard/app/lib/actions.ts
@@ -26,6 +26,7 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 // Action d'ajout d'une facture en BDD
 export async function createInvoice(formData: FormData) {
 
+
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
@@ -35,13 +36,22 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100; // Convertion en centimes, pour éliminer les erreurs de virgule flottante JavaScript et garantir une plus grande précision.
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+
+    } catch( error ){
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };   
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+    // Redirect est appelé en dehors du try/catch bloc. Car cela génére une erreur, qui est interceptée par le catch bloc. 
 
 }
 
@@ -54,19 +64,32 @@ export async function updateInvoice(id: string, formData: FormData) {
     });
     
     const amountInCents = amount * 100;
-    
-    await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-    `;
-    
+
+    try{
+        await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+        `;
+    } catch (error) {
+        return { message: 'Database Error: Failed to Update Invoice.' };
+    }
+
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 // Action de suppresion d'une facture en BDD
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
+
+    // throw new Error('Failed to Delete Invoice');
+    // Bloc de code inaccessible du a l'erreur généré
+
+    try{
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return { message: 'Deleted Invoice.' };
+    } catch (error) {
+        return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
 }
