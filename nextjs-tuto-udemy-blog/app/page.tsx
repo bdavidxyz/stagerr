6c2de013529bb83f3580fcbd9572a3825383c8cb
Renaud Redron <renaud.redron@gmail.com>
2024-08-21 11:30:48 +0200

    Navigation : Link et hook useRouter()

--- nextjs-tuto-udemy-blog/app/page.tsx
+++ nextjs-tuto-udemy-blog/app/page.tsx
@@ -1,12 +1,28 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { Eye } from "lucide-react"
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter()
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button variant="outline" >
-        <Eye size={24} />
-        Click me</Button>
+      <Button 
+        variant="outline" 
+        onClick={() => router.push("/login")} 
+      >
+        <Eye size={24} /> Login
+      </Button>
+
+      <Button 
+        variant="outline" 
+        onClick={() => router.push("/categories/react")} 
+      >
+        <Eye size={24} /> React
+      </Button>
     </main>
   );
 }
