19d8befa1aa9df72fdda39f029ef4a4f7af4f14b
Renaud Redron <renaud.redron@gmail.com>
2024-08-22 14:21:02 +0200

    Front : Mise en place de la fonction de connexion et de déco a l'aide de hoot useSession

--- nextjs-tuto-udemy-blog/app/login/page.tsx
+++ nextjs-tuto-udemy-blog/app/login/page.tsx
@@ -1,20 +1,28 @@
+"use client"
+
 import PageContainer from "@/components/page-container";
 import PageTitle from "@/components/page-title";
 import { Button } from "@/components/ui/button";
 import { Github, Mail } from "lucide-react";
+import { signIn } from "next-auth/react";
 
 export default function LoginPage() {
+
+    const onLogin = (provider : string) => {
+        signIn(provider)
+    }
+
     return (
         <PageContainer>
             <div className="p-10">
                 <PageTitle title="Login or Register" />
 
                 <div className="flex flex-col gap-4 max-w-sm mx-auto">
-                    <Button>
+                    <Button onClick={() => onLogin("github")}>
                         <Github className="mr-3" />
                         Signin with Github
                     </Button>
-                    <Button>
+                    <Button onClick={() => onLogin("google")}>
                         <Mail className="mr-3" />
                         Signin with Google
                     </Button>
--- nextjs-tuto-udemy-blog/components/profile-button.tsx
+++ nextjs-tuto-udemy-blog/components/profile-button.tsx
@@ -1,15 +1,47 @@
+"use client"
+
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 
 export default function ProfileButton() {
-    // L'utilisateur est pas connecté => bouton login
 
-    return (
-        <Link href="/login">
-            <Button>Login/Signup</Button>
-        </Link>
-    )
+    const {data: session, status} = useSession()
+
+    // L'utilisateur est pas connecté => bouton login
+    // status == "unauthenticated" <---- Equivalent a !session
+    if (!session){
+        return (
+            <Link href="/login">
+                <Button>Login/Signup</Button>
+            </Link>
+        )
+    }
+
+    const onLogout = () => {
+        signOut()
+    }
 
     // L'utilisateur est connecté => avatar + menu
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                    <AvatarImage src={session.user?.image || "/img/shadcn.jpeg"} />
+                    <AvatarFallback>{session.user?.name}</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem 
+                    onClick={() => onLogout()}
+                    className="cursor-pointer"
+                >
+                    Log out
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
 
 }
